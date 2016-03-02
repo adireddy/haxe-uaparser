06c3fad47025c654a3c74d7950c021d3ca6c06eb
Adi <adiinteractive@gmail.com>
2016-03-02 10:28:55 +0000

    new release

--- src/UAParser.hx
+++ src/UAParser.hx
@@ -1,7 +1,14 @@
 @:native("UAParser")
 extern class UAParser {
 
-    function new():Void;
+    static var VERSION:String;
+    static var BROWSER:Dynamic;
+    static var CPU:Dynamic;
+    static var DEVICE:Dynamic;
+    static var ENGINE:Dynamic;
+    static var OS:Dynamic;
+
+    function new(?ua:String, ?extensions:Dynamic):Void;
 
     function getBrowser():BrowserObject;
 
